9c7245e739fe850e5d6e3352e48712acc60dd8dc
shyam <shyamsundar.shanmugam@wipro.com>
2018-03-27 10:26:24 +0800

    extracting message as an instance var

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,17 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @Value("${WELCOME_MESSAGE}")
+    String message;
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
