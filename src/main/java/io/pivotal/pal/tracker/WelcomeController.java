2fa2199978dec47f35f92c7b6de1ba24955aa18c
shyam <shyamsundar.shanmugam@wipro.com>
2018-03-27 11:27:18 +0800

    adding unit tests for env and welcome controller

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @Value("${WELCOME_MESSAGE}")
+
     String message;
+
+    public WelcomeController(
+            @Value("${WELCOME_MESSAGE}") String message
+    ) {
+        this.message = message;
+    }
 
     @GetMapping("/")
     public String sayHello() {
