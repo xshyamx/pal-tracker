db1796f6d58804a5d23fc143e17e8019f812fa9d
apivakim <nuva_apiva@outlook.co.th>
2018-04-02 09:35:32 +0800

    fixing jdbc repo and updating manifest files to include database service

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -5,9 +5,7 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class JdbcTimeEntryRepository implements TimeEntryRepository {
@@ -21,21 +19,23 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
     @Override
     public TimeEntry create(TimeEntry any) {
         KeyHolder holder = new GeneratedKeyHolder();
-        template.update(
-                "insert into time_entries(project_id,user_id,date,hours) values(?,?,?,?)",
-                new PreparedStatementSetter() {
-
-                    @Override
-                    public void setValues(PreparedStatement ps) throws SQLException {
-                        int idx = 1;
-                        ps.setLong(idx++, any.getProjectId());
-                        ps.setLong(idx++, any.getUserId());
-                        ps.setDate(idx++, Date.valueOf(any.getDate()));
-                        ps.setInt(idx++, any.getHours());
-                    }
-                }, holder
-        );
-        return find(holder.getKey().longValue());
+        int update = template.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement("insert into time_entries(project_id,user_id,date,hours) values(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+                int idx = 1;
+                ps.setLong(idx++, any.getProjectId());
+                ps.setLong(idx++, any.getUserId());
+                ps.setDate(idx++, Date.valueOf(any.getDate()));
+                ps.setInt(idx++, any.getHours());
+                return ps;
+            }
+        }, holder);
+        if ( update > 0 ) {
+            return find(holder.getKey().longValue());
+        } else {
+            throw new RuntimeException("Failed to insert into time_entries");
+        }
     }
 
     @Override
@@ -47,17 +47,52 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
 
     @Override
     public List<TimeEntry> list() {
-        return template.queryForList("SELECT id,project_id,user_id,date,hours FROM time_entries", TimeEntry.class);
+        return template.query("SELECT id,project_id,user_id,date,hours FROM time_entries", mapper);
     }
 
     @Override
-    public TimeEntry update(long eq, TimeEntry any) {
-        return null;
+    public TimeEntry update(long id, TimeEntry any) {
+        TimeEntry toUpdate = find(id);
+        if ( toUpdate != null ) {
+            toUpdate.setProjectId(any.getProjectId());
+            toUpdate.setUserId(any.getUserId());
+            toUpdate.setDate(any.getDate());
+            toUpdate.setHours(any.getHours());
+            int updated = template.update(
+                    "update time_entries set project_id=?, user_id=?, date=?, hours=? where id=?",
+                    new PreparedStatementSetter() {
+                        @Override
+                        public void setValues(PreparedStatement ps) throws SQLException {
+                            int idx = 1;
+                            ps.setLong(idx++, toUpdate.getProjectId());
+                            ps.setLong(idx++, toUpdate.getUserId());
+                            ps.setDate(idx++, Date.valueOf(toUpdate.getDate()));
+                            ps.setInt(idx++, toUpdate.getHours());
+                            ps.setLong(idx++, id);
+                        }
+                    }
+            );
+            if ( updated > 0 ) {
+                return toUpdate;
+            } else {
+                throw new RuntimeException("Failed to update time entry");
+            }
+        }
+        return toUpdate;
     }
 
     @Override
-    public TimeEntry delete(long l) {
-        return null;
+    public TimeEntry delete(long id) {
+        TimeEntry toDelete = find(id);
+        if ( toDelete != null ) {
+            int deleted = template.update("delete from time_entries where id=?", id);
+            if ( deleted > 0 ) {
+                return  toDelete;
+            } else {
+                throw new RuntimeException("Failed to delete time entry");
+            }
+        }
+        return toDelete;
     }
 
     private final RowMapper<TimeEntry> mapper = (rs, rowNum) -> new TimeEntry(
